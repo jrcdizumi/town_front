4e310170e190c78dca10c8fb66c54bee9fdd0cfb
jrcdizumi <cd15382858999@163.com>
2024-12-05 17:34:53 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login.vue';
+
+// ...existing code...
+
+const routes = [
+  // ...existing routes...
+  {
+    path: '/user/login',
+    name: 'Login',
+    component: Login
+  }
+];
+
+// ...existing code...
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="login-container">
+      <el-form :model="loginForm" @submit.prevent="handleLogin">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" placeholder="密码" type="password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleLogin">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </template>
+  
+  <script>
+  import { ElMessage } from 'element-plus';
+  
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        loginForm: {
+          username: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      handleLogin() {
+        this.$axios.post('http://172.22.99.79:8080/user/login', {
+          uname: this.loginForm.username,
+          bpwd: this.loginForm.password
+        }).then(response => {
+          if (response.data.code === 200) {
+            ElMessage.success('登录成功');
+            localStorage.setItem('token', response.data.data.token);
+            this.$router.push('/');
+          } else {
+            ElMessage.error(response.data.message);
+          }
+        }).catch(error => {
+          console.error(error);
+        });
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .login-container {
+    width: 300px;
+    margin: 100px auto;
+  }
+  </style>
