2646f10c52c2abb4658b5a5543f64badc3baaaa9
hdk <15060659+dkhu@user.noreply.gitee.com>
2024-12-25 18:39:19 +0800

    修改为三级选择框

--- src/views/Statistic copy.vue
+++ src/views/Statistic copy.vue
@@ -1,76 +1,152 @@
 <template>
-    <div class="statistic-container">
-      <el-form :model="loginForm" @submit.prevent="handleLogin">
-        <el-form-item prop="username">
-          <el-input v-model="loginForm.username" placeholder="用户名"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="loginForm.password" placeholder="密码" type="password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="handleLogin">管理员登录</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
-  </template>
-  
-  <script>
-  import { ElMessage } from 'element-plus';
-  import { onMounted } from 'vue'
-  import axios from 'axios'
-  
-  export default {
-    name: 'Statistic',
-    data() {
-      return {
-        loginForm: {
-          username: '',
-          password: ''
-        }
-      }
-    },
-    mounted() {
-      const token = localStorage.getItem('token')
-      if (token) {
-        this.$axios.get('http://localhost:8080/user/checkstatistic', {
-          headers: {
-            'token': token
-          }
-        }).then(response => {
-          const res = response.data
-          if (res.code === 200) {
-            // 已登录，跳转到 home 界面
-            this.$router.push('/')
-          }
-        }).catch(error => {
-          console.error('验证失败', error)
-        })
-      }
-    },
-    methods: {
-      handleLogin() {
-        this.$axios.post('http://localhost:8080/user/statistic', {
-          uname: this.loginForm.username,
-          bpwd: this.loginForm.password
-        }).then(response => {
+  <div class="statistic-container">
+    <el-form :model="filterForm" label-width="120px" @submit.prevent="fetchStatistics">
+      <el-form-item label="起始年月">
+        <el-date-picker
+          v-model="filterForm.startDate"
+          type="month"
+          placeholder="选择起始年月"
+          format="YYYY-MM"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="终止年月">
+        <el-date-picker
+          v-model="filterForm.endDate"
+          type="month"
+          placeholder="选择终止年月"
+          format="YYYY-MM"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="乡镇">
+        <el-cascader :options="provinces" placeholder="请选择省份/城市/乡镇" v-model="filterForm.address" clearable
+          :props="{
+            expandTrigger: 'hover',
+            children: 'children',
+            emitPath: true,
+            showAllLevels: false,
+            value: 'value'
+          }"
+        ></el-cascader>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="fetchStatistics">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table :data="statistics" style="width: 100%; margin-top: 20px;">
+      <el-table-column prop="month" label="月份"></el-table-column>
+      <el-table-column prop="promotionUsers" label="宣传用户数"></el-table-column>
+      <el-table-column prop="supportUsers" label="助力用户数"></el-table-column>
+    </el-table>
+
+    <div id="chart" style="width: 100%; height: 400px; margin-top: 20px;"></div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts';
+import axios from 'axios';
+
+export default {
+  name: 'Statistic',
+  setup() {
+    const filterForm = ref({
+      startDate: '',
+      endDate: '',
+      address: []
+    });
+
+    const provinces = ref([]);
+    const statistics = ref([]);
+
+    const fetchTowns = () => {
+      return axios.get('http://localhost:8080/publicize/getTownList')
+        .then(response => {
           if (response.data.code === 200) {
-            ElMessage.success('success');
-            localStorage.setItem('token', response.data.data.token);
-            this.$router.push('/');
+            provinces.value = JSON.parse(response.data.data);
           } else {
-            ElMessage.error(response.data.message);
+            throw new Error(response.data.message || '获取乡镇信息失败');
           }
-        }).catch(error => {
-          console.error(error);
         });
-      }
-    }
+    };
+
+    const fetchStatistics = () => {
+      const townID = filterForm.value.address[filterForm.value.address.length - 1];
+      axios.get('http://localhost:8080/statistics', {
+        params: {
+          startDate: filterForm.value.startDate,
+          endDate: filterForm.value.endDate,
+          townID: townID
+        }
+      }).then(response => {
+        if (response.data.code === 200) {
+          statistics.value = response.data.data;
+          renderChart();
+        } else {
+          console.error('查询失败', response.data.message);
+        }
+      }).catch(error => {
+        console.error('查询失败', error);
+      });
+    };
+
+    const renderChart = () => {
+      const chart = echarts.init(document.getElementById('chart'));
+      const months = statistics.value.map(item => item.month);
+      const promotionUsers = statistics.value.map(item => item.promotionUsers);
+      const supportUsers = statistics.value.map(item => item.supportUsers);
+
+      const option = {
+        title: {
+          text: '宣传情况月汇总分析'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['宣传用户数', '助力用户数']
+        },
+        xAxis: {
+          type: 'category',
+          data: months
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: '宣传用户数',
+            type: 'line',
+            data: promotionUsers
+          },
+          {
+            name: '助力用户数',
+            type: 'line',
+            data: supportUsers
+          }
+        ]
+      };
+
+      chart.setOption(option);
+    };
+
+    onMounted(() => {
+      fetchTowns();
+    });
+
+    return {
+      filterForm,
+      provinces,
+      statistics,
+      fetchStatistics
+    };
   }
-  </script>
-  
-  <style scoped>
-  .statistic-container {
-    width: 300px;
-    margin: 100px auto;
-  }
-  </style>
+};
+</script>
+
+<style scoped>
+.statistic-container {
+  padding: 20px;
+}
+</style>
